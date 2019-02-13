96700297905fdcd75b612eb217c4f3027469ced9
Thesk0rn <47185037+Thesk0rn@users.noreply.github.com>
2019-02-13 13:46:22 +0100

    Update main.c
    Added random passwords generation.
    Modified Genere function so it works better.
    Deleted writeFile function because it was useless.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
 #include <math.h>
 
 #define DEFAULT_FILE "password.txt"
@@ -13,14 +14,18 @@
 
 void man(void);
 
-void writeFile(char str, char* fileName);
-void Genere(int NbCharMax, char* fileName);
+int writeFile(char str, const char* fileName);
+int strFile(char *str, const char* filename);
+
+int Genere(int NbCharMax, const char* fileName);
+int randGen(int nbPassword, int maxPass, const char* fileName);
 uint64_t numPass(int length);
 
 int main(int argc, char** argv)
 {
     int validArg = 0;
     char* fileName = NULL;
+    int maxPass = 0;
 
     if(argc < 2)
     {
@@ -44,6 +49,12 @@ int main(int argc, char** argv)
     {
         for(int i = 1; i<argc; i++)
         {
+            if((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "-h") == 0))
+            {
+                man();
+                return EXIT_SUCCESS;
+            }
+
             if(strcmp(argv[i], "-o") == 0)
             {
                 if(argc>=i+1)
@@ -66,12 +77,48 @@ int main(int argc, char** argv)
                 for(int j = 1; j < atoi(argv[i+1])+1; j++)
                     nbPassword += numPass(j);
 
-                printf("%d passwords will be generated.\n", nbPassword);
+                if(fileName != NULL)
+                {
+                    if(Genere(atoi(argv[i+1]), fileName) == -1)
+                    {
+                        printf("Error opening file.\n");
+                        return EXIT_FAILURE;
+                    }
+                }
+                else
+                {
+                    if(Genere(atoi(argv[i+1]), DEFAULT_FILE) == -1)
+                    {
+                        printf("Error opening file.\n");
+                        return EXIT_FAILURE;
+                    }
+                }
+            }
+
+            if(strcmp(argv[i], "-r") == 0)
+            {
+                validArg = 1;
+                int nbPassword = atoi(argv[i+1]);
+
+                maxPass  = atoi(argv[i+2]);
 
                 if(fileName != NULL)
-                    Genere(atoi(argv[i+1]), fileName);
+                {
+                    if(randGen(nbPassword, maxPass, fileName) == -1)
+                    {
+                        printf("Error opening file.\n");
+                        return EXIT_FAILURE;
+                    }
+                }
                 else
-                    Genere(atoi(argv[i+1]), DEFAULT_FILE);
+                {
+                    if(randGen(nbPassword, maxPass, DEFAULT_FILE) == -1)
+                    {
+                        printf("Error opening file.\n");
+                        return EXIT_FAILURE;
+                    }
+                }
+
             }
         }
 
@@ -80,6 +127,10 @@ int main(int argc, char** argv)
             man();
             return EXIT_SUCCESS;
         }
+        else
+        {
+            free(fileName);
+        }
     }
     else
     {
@@ -87,38 +138,29 @@ int main(int argc, char** argv)
         return EXIT_SUCCESS;
     }
 
+    free(fileName);
     return EXIT_SUCCESS;
 }
 
-void writeFile(char str, char* fileName){
-    FILE* fichier = NULL;
-
-    fichier = fopen(fileName, "a");
-    fputc(str, fichier);
-
-    fclose(fichier);
-}
-
-void Genere(int NbCharMax, char* fileName)
+int Genere(int NbCharMax, const char* fileName)
 {
   char *mdp = NULL;
   mdp = malloc(NbCharMax*sizeof(char));
 
   int nbPass = 0;
-
   FILE* fichier = NULL;
 
-  fichier = fopen(fileName, "w");
-  fclose(fichier);
-
   fichier = fopen(fileName, "a");
+
+  if(fichier == NULL)
+      return -1;
 
   for(int nbChar=0;nbChar<NbCharMax;nbChar++)
   {
     int cursorCara = 0;
     mdp = realloc(mdp, (nbChar+1)*sizeof(char));
 
-    for(int compt = 0; compt <=nbChar; compt++)
+    for(int compt = 0; compt<=nbChar; compt++)
       mdp[compt] = DEBUT_ASCII;
 
     for(cursorCara=nbChar; cursorCara>=0; cursorCara--)
@@ -127,15 +169,12 @@ void Genere(int NbCharMax, char* fileName)
        {
          mdp[cursorCara] = cursorAscii;
 
-         for(int i=0;i<=nbChar;i++)
-           writeFile(mdp[i], fileName);
-
-         writeFile('\n', fileName);
+         if(fprintf(fichier, "%s\n", mdp) < 0)
+             return -2;
 
          ++nbPass;
 
-         erase();
-         printf("%d passwords created.\n", nbPass);
+         printf("%d: %s\n", nbPass, mdp);
        }
 
        if(cursorCara > 0)
@@ -152,6 +191,43 @@ void Genere(int NbCharMax, char* fileName)
        }
     }
   }
+
+  fclose(fichier);
+  free(mdp);
+
+  return 0;
+}
+
+int randGen(int nbPassword, int maxPass, const char* fileName)
+{
+    int character = 0;
+    char password[maxPass];
+
+    FILE* fichier = NULL;
+    fichier = fopen(fileName, "a");
+
+    if(fichier == NULL)
+        return -1;
+
+    srand(time(NULL));
+
+    for(int i=0; i<nbPassword; ++i)
+    {
+        for(int j=0; j<maxPass; ++j)
+        {
+            character = (rand()%(FIN_ASCII - DEBUT_ASCII + 1)) + DEBUT_ASCII;
+            password[j] = character;
+        }
+
+        if(fprintf(fichier, "%s\n", password) < 0)
+            return -2;
+
+        printf("%d: %s\n", i+1, password);
+    }
+
+    fclose(fichier);
+
+    return 0;
 }
 
 uint64_t numPass(int length)
@@ -162,8 +238,11 @@ uint64_t numPass(int length)
 void man(void)
 {
     printf("Usage : passGen [OPTION]...\n");
-    printf("        passGen -o <file> -n <number>\n\n");
-    printf("-o <file>         Change output file for <file>. 'password.txt' is the default output if none is specified\n");
-    printf("-n <number>       Create passwords for <number> max char\n");
-    printf("-h or --help      Display the help menu\n");
+    printf("Exemples:\n");
+    printf("        passGen -o passlist -n 4\n");
+    printf("        passgen -r 1000 5\n\n");
+    printf("-o <file>                      Change output file for <file>. 'password.txt' is the default output if none is specified\n");
+    printf("-n <number>                    Create passwords for <number> max char\n");
+    printf("-r <number> <length>           Create <number> random passwords of <length> characters\n");
+    printf("-h or --help                   Display the help menu\n");
 }
